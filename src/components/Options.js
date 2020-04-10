add07e5c66815802e64cc69f6cd4c0e687e06627
Cesar Morales <cesarlenin2@live.com>
2020-04-10 11:02:40 -0700

    first division

---
+++ src/components/Options.js
@@ -0,0 +1,9 @@
+// // import React, { Component } from 'react';
+// // import slugify from 'slugify';
+
+
+// class Options extends Component {
+    
+// }
+
+// export default Options;
---
+++ src/components/Summary.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+
+class Summary extends Component {
+    render() {
+         const summary = Object.keys(this.props.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.props.selected[feature];
+      
+            return (
+              <div className="summary__option" key={featureHash}>
+                <div className="summary__option__label">{feature} </div>
+                <div className="summary__option__value">{selectedOption.name}</div>
+                <div className="summary__option__cost">
+                  {this.props.usCurrencyFormat.format(selectedOption.cost)}
+                </div>
+              </div>
+            );
+          });
+          return summary
+    }
+}
+
+
+export default Summary;
